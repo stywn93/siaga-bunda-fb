f55fdd77f73096c4edd55a86f96809ecf6665664
Iwan Bagus S <stywn93@gmail.com>
2026-09-22 10:32:59 +0700

    Refactor MomProfile component to include risk prop and update Content layout

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,20 +5,12 @@ function Content(){
         <>
         <div className="p-4">
                 <div className="p-4 border-1 border-default border-dashed rounded-base">
-                    <div className="grid grid-cols-3 gap-4 mb-4">
-                        <MomProfile color="#04B8AC"/>
-                        <div className="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                            <p className="text-fg-disabled">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" /></svg>
-                            </p>
-                        </div>
-                        <div className="flex items-center justify-center h-24 rounded-base bg-neutral-secondary-soft">
-                            <p className="text-fg-disabled">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" /></svg>
-                            </p>
-                        </div>
+                    <div className="grid grid-cols-3 gap-2 mb-4">
+                        <MomProfile color="#b91c1c" risk="high"/>
+                        <MomProfile />
+                        <MomProfile />
                     </div>
-                    <div className="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
+                    {/* <div className="flex items-center justify-center h-48 rounded-base bg-neutral-secondary-soft mb-4">
                         <p className="text-fg-disabled">
                             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" /></svg>
                         </p>
@@ -71,7 +63,7 @@ function Content(){
                                 <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" /></svg>
                             </p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
--- src/components/MomProfile.tsx
+++ src/components/MomProfile.tsx
@@ -1,12 +1,13 @@
-import {UserRound} from "lucide-react"
+import { UserRound, MapPin, Form } from "lucide-react"
 type Props = {
-    color: string
+    color?: string,
+    risk?: string
 }
-function MomProfile({color}: Props) {
+function MomProfile({color = "#000000", risk = "normal"}: Props) {
     return (
         
         <>
-            <div className="relative bg-neutral-primary-soft max-w-xs w-full p-6 border border-default rounded-base shadow-xs">
+            <div className="relative bg-neutral-primary-soft max-w-s w-full p-2 border border-default rounded-base shadow-xs">
                 <button id="dropdownButton" data-dropdown-toggle="dropdown" className="absolute top-2 end-2 text-body hover:text-heading bg-neutral-primary-soft box-border border border-transparent hover:bg-neutral-tertiary focus:ring-4 focus:ring-neutral-tertiary rounded-base p-1.5 focus:outline-none" type="button">
                     <span className="sr-only">Open dropdown</span>
                     <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M6 12h.01m6 0h.01m5.99 0h.01" /></svg>
@@ -27,17 +28,25 @@ function MomProfile({color}: Props) {
                 </div>
                 <div className="flex flex-col items-center">
                     <UserRound className="w-24 h-24 mb-6 rounded-full" color={color}/>
+
+                    <span className={`inline-flex items-center ${risk === "high" ? "bg-danger-softer" : "bg-brand-softer"} border ${risk === "high" ? "border-danger-subtle" : "border-brand-subtle"} ${risk === "high" ? "text-dg-danger-strong" : "text-dg-brand-strong"} text-xs font-medium px-1.5 py-0.5 rounded-sm`}>
+                        <svg className="w-3 h-3 me-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.122 17.645a7.185 7.185 0 0 1-2.656 2.495 7.06 7.06 0 0 1-3.52.853 6.617 6.617 0 0 1-3.306-.718 6.73 6.73 0 0 1-2.54-2.266c-2.672-4.57.287-8.846.887-9.668A4.448 4.448 0 0 0 8.07 6.31 4.49 4.49 0 0 0 7.997 4c1.284.965 6.43 3.258 5.525 10.631 1.496-1.136 2.7-3.046 2.846-6.216 1.43 1.061 3.985 5.462 1.754 9.23Z"/></svg>
+                        {risk === "high" ? "Resiko Tinggi" : "Normal"}
+                    </span>
                     {/* <img className="w-24 h-24 mb-6 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" /> */}
                     <h5 className="mb-0.5 text-xl font-semibold tracking-tight text-heading">Ibu Fulanah</h5>
                     <span className="text-sm text-body">35 minggu</span>
-                    <span className="bg-danger-soft text-fg-danger-strong text-xs font-medium px-1.5 py-0.5 rounded">Danger</span>
+                    <p className="text-body inline-flex items-center gap-1">
+                        <MapPin className="w-4 h-4" />
+                        <span>Curah Jeru, Panji.</span>
+                    </p>
                     <div className="flex mt-4 md:mt-6 gap-4">
                         <button type="button" className="inline-flex items-center text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-                            <svg className="w-4 h-4 me-1.5 -ms-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" /></svg>
-                            Follow me
+                            <Form className="size-3.5 mr-1.5"/>
+                            Lihat Profil Risiko
                         </button>
                         <button type="button" className="inline-flex self-start w-auto text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
-                            Message
+                            Ikuti Perjalanan
                         </button>
                     </div>
                 </div>

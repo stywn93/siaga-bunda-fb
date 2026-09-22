9c603e43ea6fc45facf064807303b02d76d4be76
Iwan Bagus S <stywn93@gmail.com>
2026-09-22 08:57:07 +0700

    Add routing with react-router-dom and create AppShell and Content components

---
+++ src/AppShell.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar"
+
+function AppShell() {
+    return(
+        <>
+        <Sidebar/>
+        </>
+    )
+}
+
+export default AppShell
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Sidebar from './components/Sidebar.tsx'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import AppShell from './AppShell.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Sidebar />
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<AppShell/>}/>
+      </Routes>
+    </HashRouter>
   </StrictMode>
 )

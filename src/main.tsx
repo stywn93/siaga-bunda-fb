bf7e2ed1c0348cf472453bfeea62ec1de39eb089
Iwan Bagus S <stywn93@gmail.com>
2026-09-22 08:18:28 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Sidebar from './components/Sidebar.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Sidebar />
+  </StrictMode>
+)
